edd36ee050d808c66f1fb42f6bcc1c916526efa5
RogueBukkitDev <audiatorix@gmail.com>
2015-08-07 16:09:17 -0400

    Some formatting

--- src/skystrike/antraxrpg/AntraxRPG.java
+++ src/skystrike/antraxrpg/AntraxRPG.java
@@ -2,14 +2,18 @@ package skystrike.antraxrpg;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class AntraxRPG extends JavaPlugin {
+
+public class AntraxRPG extends JavaPlugin
+{
     @Override
-    public void onEnable() {
-        getLogger().info("AntraxRPG Starting up");
+    public void onEnable()
+    {
+
     }
 
     @Override
-    public void onDisable() {
-        getLogger().info("AntraxRPG Shutting down");
+    public void onDisable()
+    {
+
     }
 }
